06cbd4d0644e41d3f8a15f1d1d0123c3e30d2ebd
shivanshi <shivanshi.saxena2@gmail.com>
2023-04-09 13:59:30 +0530

    ui for typing and result display

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { faker } from '@faker-js/faker';
 import RestartButton from './components/RestartButton';
+import Results from './components/Results';
+import UserTypings from './components/UserTypings';
 
 const words = faker.random.words(10)
 
@@ -8,18 +10,38 @@ function App() {
   return (
     <>
       <CountdownTimer timeLeft={30} />
-      <GeneratedWords words={words} />
+
+      <WordsContainer>
+          <GeneratedWords words={words} />
+          <UserTypings className='absolute inset-0' userInput={"test"} />
+      </WordsContainer>
+
+      
       <RestartButton
           className={"mx-auto mt-10 text-slate-500"}
           onRestart={() => null}
       />
-      
+      <Results
+          className='mt-10'
+          errors={10}
+          accuracyPercentage={100}
+          total={200}
+      />
     </>
   );
 };
 
+const WordsContainer = ({children } : {children : React.ReactNode}) => {
+  return (
+    <div className='relative max-w-xl mt-3 text-3xl leading-relaxed break-all'>
+        {children}
+    </div>
+  )
+}
+
+
 const GeneratedWords = ({words} : {words: string}) => {
-  return <div className='text-4xl text-center text-slate-400'>{words}</div>
+  return <div className='text-slate-400'>{words}</div>
 }
 
 const CountdownTimer = ({timeLeft} : {timeLeft: number}) => {
---
+++ src/components/Caret.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Caret = () => {
+  return (
+    <div>Caret</div>
+  )
+}
+
+export default Caret
